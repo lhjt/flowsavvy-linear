becf72ba7acfb7bf34555fd08c19f47f9da7a978
Jared L <48422312+lhjt@users.noreply.github.com>
2025-05-09 22:02:06 +1000

    chore: generate first snapshot sdk

---
+++ packages/flowsavvy-sdk/src/apis/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AccountApi';
+export * from './CalendarApi';
+export * from './InboxApi';
+export * from './ItemApi';
+export * from './ScheduleApi';
+export * from './TagApi';
